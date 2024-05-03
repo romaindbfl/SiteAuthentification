3489c6f613d1270c8cca881fd3ec4b8b14e06e6e
romaindbfl <romain.deboffle2308@gmail.com>
2024-05-03 20:50:16 +0200

    Final

--- Exercice10/bandit.php
+++ Exercice10/bandit.php
@@ -36,7 +36,7 @@ include_once 'verif.php';
     <!-- Liste des commentaires -->
     <div class="comments-list">
         <?php
-        $imageId = 1; 
+        $imageId = 3; 
         // Inclure le code PHP pour afficher les commentaires
         include 'commentaires.php';
         ?>
